001d9b306b1287ec8c77e6270a3fd06271470b1c
Yerassyl <yerassyl88@gmail.com>
2021-10-06 02:31:30 +0600

    code refactoring

--- Week1/Hackerrank/h.py
+++ Week1/Hackerrank/h.py
@@ -1,12 +1,10 @@
 if __name__ == '__main__':
 
     n = int(input())
-    
     arr = [int(x) for x in input().split()]
-
-    maxi = max(arr)
+    max_ = max(arr)
 
     for i in sorted(arr)[::-1]:
-        if i != maxi:
+        if i != max_:
             print(i)
             break
